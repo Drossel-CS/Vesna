6a6c74689b0f556a54f8da2b9c7c4eecc02e51d4
NagyDRCS <42371686+NagyDRCS@users.noreply.github.com>
2018-11-05 20:09:13 +0100

    Vesna upload
    bazinga

---
+++ footer.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Vesna
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="wrapp">
+			<div class="site-footer-brands">
+				<?php dynamic_sidebar( 'Brands-1' ) ?>
+			</div>
+			<div class="site-footer-menus">
+				<?php dynamic_sidebar( 'footer-menu-1' ) ?>
+				<?php dynamic_sidebar( 'footer-menu-2' ) ?>
+				<?php dynamic_sidebar( 'footer-menu-3' ) ?>
+				<?php dynamic_sidebar( 'footer-menu-4' ) ?>
+			</div>
+			<div class="site-footer-social">
+				<a href="https://www.instagram.com/gustoe_kolaciky/" target="_blank" title="Instagram"><i class="icon-instagram"></i></a>
+				<a href="https://www.facebook.com/Gustoe.sk/" target="_blank"><i class="icon-facebook-squared"></i></a>
+				<i class="icon-youtube"></i>
+			</div>
+			<div class="site-footer-copy">
+				<p class="copy">&copy<?php echo date(' Y '); bloginfo('name'); ?></p>
+				<p class="author">Vytvoril <a href="https://www.drossel.sk" target="_blank">Drossel</a></p>
+			</div>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Vesna
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'vesna' ); ?></a>
+
+	<header id="masthead" class="site-header">
+		<div class="site-branding wrapp">
+			<div class="site-header-logo">
+				<?php the_custom_logo();?>
+			</div>
+			<div class="site-header-primary-nav">
+				<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'vesna' ); ?></button>
+					<?php
+					wp_nav_menu( array(
+						'theme_location' => 'header-menu',
+						'menu_id'        => 'primary-menu',
+					) );
+					?>
+				</nav><!-- #site-navigation -->
+			</div>
+		</div><!-- .site-branding -->
+		
+	</header><!-- #masthead -->
+	
+
+	<div id="content" class="site-content">
