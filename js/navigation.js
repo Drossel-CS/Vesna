70841dd88fdd4e66ea324fcb98d5c570a9b7c01f
Peter Kriška <pekriska@gmail.com>
2018-11-07 17:08:08 +0100

    menu update

--- js/navigation.js
+++ js/navigation.js
@@ -7,10 +7,9 @@
 $ = jQuery.noConflict(true);
 
 $(document).ready( function () {
-
+$("#primary-menu").css("display","none");
 $('.menu-toggle').click(function(e) {
    e.preventDefault();
-   
     if ($('#primary-menu').is(":visible") ) {
       $('#primary-menu').stop(true,true).slideUp(1000);
     } else {
