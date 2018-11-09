de0c17cc14f8352cab0cbae526c807211ae99d84
Peter Kriška <pekriska@gmail.com>
2018-11-09 13:01:08 +0100

    menu opravene
    Nespravne bolo vytvorene menu vo function php

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
 				<nav id="site-navigation" class="main-navigation">
 					<?php
 					wp_nav_menu( array(
-						'theme_location' => 'header-menu',
+						'theme_location' => 'menu-1',
 						'menu_id'        => 'primary-menu',
 						'menu_class' => 'menu-items',
 					) );
