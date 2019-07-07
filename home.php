02075e7ff838485d550ec457e86b9d24cbed5874
Peter Nagy <42371686+NagyDRCS@users.noreply.github.com>
2019-07-07 18:07:53 +0200

    1.00.18

--- home.php
+++ home.php
@@ -7,19 +7,19 @@
 		<main id="main" class="site-main wrapp">
 
     <?php if ( have_posts() ) : ?>
-        <?php while ( have_posts() ) : the_post(); ?>
+            <?php while ( have_posts() ) : the_post(); ?>
 
-            <div <?php post_class() ?> class="post-container">
-            <?php
-            if ( has_post_thumbnail() ) {
-                the_post_thumbnail();
-            } 
-            ?>
-            <h3><?php the_title() ?></h3>
-            <a href="<?php echo get_permalink( get_the_ID() ); ?>"><button class="read-more">Prečítať článok</button></a>
-            </div>
+                <div <?php post_class('post-container') ?>>
+                <?php
+                if ( has_post_thumbnail() ) {
+                    the_post_thumbnail();
+                } 
+                ?>
+                <h3><?php the_title() ?></h3>
+                <a href="<?php echo get_permalink( get_the_ID() ); ?>"><button class="read-more">Prečítať článok</button></a>
+                </div>
 
-        <?php endwhile; ?>
+            <?php endwhile; ?>
         <?php //the_posts_navigation(); ?>
         <?php the_posts_pagination( array(
                 'mid_size' => 2,
