ff144b0f54bf35d03761a25082a9d8d16ecf47ce
Peter Kriška <pekriska@gmail.com>
2020-04-16 20:24:01 +0200

    1.00.33

--- front-page.php
+++ front-page.php
@@ -1,39 +1,39 @@
 <?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Vesna
+ */
 
-    get_header();
+get_header();
 ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main wrapp">
-     <!-- <h1><?php //echo single_post_title(); ?></h1> -->
-    <?php if ( have_posts() ) : ?>
-            <?php while ( have_posts() ) : the_post(); ?>
+		<main id="main" class="site-main">
 
-                <div <?php post_class('post-container') ?>>
-                    <?php
-                    if ( has_post_thumbnail() ) {
-                        the_post_thumbnail();
-                    } 
-                    ?>
-                    <div class="post-img"><?php the_header_image_tag() ?></div>
-                    <h2 class="post-title"><?php the_title() ?></h2>
-                    <div class="post-content">
-                        <?php the_excerpt(); ?>
-                    </div>
-                    <a href="<?php echo get_permalink( get_the_ID() ); ?>"><button class="read-more">Prečítať článok</button></a>
-                </div>
+			<?php
+			while ( have_posts() ) :
+				the_post();
 
-            <?php endwhile; ?>
-        <?php //the_posts_navigation(); ?>
-        <?php the_posts_pagination( array(
-                'mid_size' => 2,
-                'prev_text' => __( '&lt', 'textdomain' ),
-                'next_text' => __( '&gt', 'textdomain' ),
-            ) ); ?>
-    <?php endif; ?>
+				get_template_part( 'template-parts/content', 'page' );
 
-    </main><!-- #main -->
-</div><!-- #primary -->
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php
 // get_sidebar();
