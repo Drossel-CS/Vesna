e3dd50a048621d820081f37c2d3052885e659f7b
Peter Kriška <pekriska@gmail.com>
2020-04-21 21:52:44 +0200

    1.01.01
    nadpisy

--- single.php
+++ single.php
@@ -12,7 +12,7 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main wrapp">
-
+		<h1><?php echo single_post_title(); ?></h1>
 		<?php
 		while ( have_posts() ) :
 			the_post();
