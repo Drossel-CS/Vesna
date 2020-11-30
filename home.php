fc9cc7f8d2acd7d3df09725420b6adea5ca0e800
Peter Nagy <nagy@drossel.sk>
2020-11-30 18:51:41 +0100

    1.01.03

--- home.php
+++ home.php
@@ -8,20 +8,20 @@
      <h1><?php echo single_post_title(); ?></h1>
     <?php if ( have_posts() ) : ?>
             <?php while ( have_posts() ) : the_post(); ?>
-
-                <div <?php post_class('post-container') ?>>
-                    <?php
-                    if ( has_post_thumbnail() ) {
-                        the_post_thumbnail();
-                    } 
-                    ?>
-                    <div class="post-img"><?php the_header_image_tag() ?></div>
-                    <h2 class="post-title"><?php the_title() ?></h2>
-                    <div class="post-content">
-                        <?php the_excerpt(); ?>
+                <a href="<?php echo get_permalink( get_the_ID() ); ?>">
+                    <div <?php post_class('post-container') ?>>
+                        <?php
+                        if ( has_post_thumbnail() ) {
+                            the_post_thumbnail();
+                        } 
+                        ?>
+                        <div class="post-img"><?php the_header_image_tag() ?></div>
+                        <h2 class="post-title"><?php the_title() ?></h2>
+                        <!-- <div class="post-content">
+                            <?php the_excerpt(); ?>
+                        </div> -->
                     </div>
-                    <a href="<?php echo get_permalink( get_the_ID() ); ?>"><button class="read-more">Prečítať článok</button></a>
-                </div>
+                </a>
 
             <?php endwhile; ?>
         <?php //the_posts_navigation(); ?>
